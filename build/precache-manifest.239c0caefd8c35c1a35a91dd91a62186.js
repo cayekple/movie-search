815543269ee4fc76a5ff79b10657b403e8c6d4a1
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2020-05-19 15:01:31 +0000

    movie search form and title completed

---
+++ build/precache-manifest.239c0caefd8c35c1a35a91dd91a62186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663e65bbf5d744035c543878855a7d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c71da2a2662d73dd81",
+    "url": "/static/css/main.ea2ad26b.chunk.css"
+  },
+  {
+    "revision": "6f8e53fdbd0793c07eda",
+    "url": "/static/js/2.a1a493f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1a493f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c71da2a2662d73dd81",
+    "url": "/static/js/main.3948e050.chunk.js"
+  },
+  {
+    "revision": "89638e70eea2c0676d80",
+    "url": "/static/js/runtime-main.ea599478.js"
+  }
+]);
